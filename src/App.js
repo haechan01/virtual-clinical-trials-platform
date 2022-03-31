6bd7c05969e6e9ab8795b5b4343232fcd1e6c47e
christopherfkk <christopherfkk@gmail.com>
2022-03-31 20:46:47 +0200

    added contract object

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Form from './Form';
 import Results from './Results';
 import Nav from './Nav'
-
-
+// import ContractLoader from './components/ContractLoader';
 
 function App() {
   return (
     <Router>
-    <Nav />
-    <div className="App">
-      <Routes>
-        <Route path="/" exact element={ <Home />} />
-        <Route path="/new-trial" element={ <Form />} />
-        <Route path="/trial-results" element={ <Results />} />
-      </Routes>
-    </div>
+      <Nav />
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/new-trial" element={<Form />} />
+          <Route path="/trial-results" element={<Results />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 const Home = () => {
-  return(
+  return (
     <div>
       <h1>
         Welcome to Clinical Trial Platform
       </h1>
-      Click "New Trial" to get started.
-      </div>
+      click "New Trial" to get started.
+    </div>
   )
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,16 @@
 import React from 'react'
-import {useState} from 'react'
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ContractPromise } from '@polkadot/api-contract'
+// import { useEffect, useState } from 'react'
+import { signCertificate, CertificateData } from '@phala/sdk'
+import { Button } from 'baseui/button'
+import { ButtonGroup } from 'baseui/button-group'
+import { toaster } from 'baseui/toast'
+import { useAtom } from 'jotai'
+import accountAtom from '../atoms/account'
+import { getSigner } from '../lib/polkadotExtension'
+
+import { useState } from 'react'
 import { useFormik } from 'formik'
 import './Form.css';
 import Papa from 'papaparse';
@@ -13,99 +24,143 @@ export default function Form() {
             dynamicTyping: true,
             complete: function (results) {
                 var data = results.data;
-                
-                if (fileType === "raw"){
+
+                if (fileType === "raw") {
                     formik.values.file = data
                 } else {
                     formik.values.file_preprocessed = data
                 }
-   
+                console.log(data)
+
             }
         });
     }
 
+
+
+    // imported Polkadot Api
+    const wsProvider = new WsProvider('ws://localhost:9944'); // local test net
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+        types
+    });
+
+    // Create a contract object with the metadata and the contract id.
+    const pruntimeURL = 'http://127.0.0.1:8000';  // assuming the default port
+    const contractId = '0xa5ef1d6cb746b21a481c937870ba491d6fe120747bbeb5304c17de132e8d0392';  // your contract id
+    const metadata = /* load the metadata.json... */;
+    const contract = new ContractPromise(
+        await create({ api, baseURL: pruntimeURL, contractId }), // Phala's "create" decorator
+        JSON.parse(metadata),
+        contractId
+    );
+
     const formik = useFormik({
-        initialValues:{
+        initialValues: {
             trialName: "",
             testType: "",
             pValueThresh: 0.05,
             file: "",
             file_preprocessed: ""
         },
+
+        // what happens when user submits the form
         onSubmit: (values) => {
-            
+
+            // try {
+            //     // initialize contract 
+            //     await contract.tx.default({})
+            //         .signAndSend(address, { signer }); // injected signer object from polkadot extension??
+            //     console.log("instantiate succeeded")
+            // }
+            // catch (e) {
+            //     console.log(e)
+            // }
+
+            try {
+                // upload raw
+                const received_p = await contract.get_p_value({});
+                console.log("received from blockchain: %s", received_p);
+            }
+            catch (e) {
+                console.log(e)
+            }
+
+
         }
     })
 
 
 
-  return (
-    <div className="container">
-    <form onSubmit={formik.handleSubmit} className="form-container">
 
-            Upload Raw Data
 
-    <div className='file-upload'>
-        <input id="file" name="file" type="file" className="upload-field" onChange={(event) => {
-  handleCSV(event.currentTarget.files[0], "raw");
-}} />
+    return (
+        <div className="container">
+            <form onSubmit={formik.handleSubmit} className="form-container">
+
+                Upload Raw Data
+
+                <div className='file-upload'>
+                    <input id="file" name="file" type="file" className="upload-field" onChange={(event) => {
+                        handleCSV(event.currentTarget.files[0], "raw");
+                    }} />
+                </div>
+                Upload Preprocessed Data
+
+                <div className='file-upload'>
+                    <input id="file_preprocessed" name="file_preprocessed" type="file" className="upload-field" onChange={(event) => {
+                        handleCSV(event.currentTarget.files[0], "processed");
+                    }} />
+                </div>
+                Give your clinical trial a name
+
+                <div className="input-block">
+                    <input
+                        className="input-field"
+                        id='trialName'
+                        name='trialName'
+                        type='text'
+                        placeholder="Trial Name"
+                        onChange={formik.handleChange}
+                        value={formik.values.trialName}
+                    />
+                </div>
+                Choose the type of test
+
+                <div className="input-block-radios">
+                    <input
+                        id='testType'
+                        name='testType'
+                        type='radio'
+                        onChange={formik.handleChange}
+                        value="fishers_exact_test"
+                    /> Fisher's Exact Test
+                    <input
+                        id='testType'
+                        name='testType'
+                        type='radio'
+                        onChange={formik.handleChange}
+                        value="meandiff"
+                    /> Difference of Means Test
+                </div>
+
+                Choose the significance level threshold
+
+                <div className="input-block">
+                    <input
+                        className="input-field"
+                        id='pValueThresh'
+                        name='pValueThresh'
+                        type='number'
+                        placeholder="0.05"
+                        onChange={formik.handleChange}
+                        value={formik.values.pValueThresh}
+                    />
+                </div>
+
+                <button type='submit' className="button" onSubmit={formik.onSubmit}>Submit</button>
+
+            </form>
         </div>
-        Upload Preprocessed Data
-
-<div className='file-upload'>
-    <input id="file_preprocessed" name="file_preprocessed" type="file" className="upload-field" onChange={(event) => {
-  handleCSV(event.currentTarget.files[0], "processed");
-}} />
-    </div>
-            Give your clinical trial a name
-
-        <div className="input-block">
-            <input
-            className="input-field"
-            id='trialName'
-            name='trialName'
-            type='text'
-            placeholder="Trial Name"
-            onChange={formik.handleChange}
-            value = {formik.values.trialName}
-             />
-        </div>
-            Choose the type of test
-
-        <div className="input-block-radios">
-            <input
-            id='testType'
-            name='testType'
-            type='radio'
-            onChange={formik.handleChange}
-            value = "fisher" 
-             /> Fisher's Exact Test
-             <input
-            id='testType'
-            name='testType'
-            type='radio'
-            onChange={formik.handleChange}
-            value = "meandiff"
-             /> Difference of Means Test
-        </div>
-        
-            Choose the significance level threshold
-  
-        <div className="input-block">
-            <input
-            className="input-field"
-            id='pValueThresh'
-            name='pValueThresh'
-            type='number'
-            placeholder="0.05"
-            onChange={formik.handleChange}
-            value = {formik.values.pValueThresh}
-             />
-        </div>
-       
-        <button type='submit' className="button">Submit</button>
-
-    </form>
-    </div>
-  )
+    )
 }
