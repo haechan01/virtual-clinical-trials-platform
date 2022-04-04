263fbe1528f8c3ff737cf3e1f4d58a52a0a6fffe
Adakole2020 <78976996+Adakole2020@users.noreply.github.com>
2022-04-04 20:21:47 +0200

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -19,13 +19,14 @@ const pruntimeURL = 'http://127.0.0.1:8000'; // assuming the default port
 const contractId = '0xa5ef1d6cb746b21a481c937870ba491d6fe120747bbeb5304c17de132e8d0392'; // your contract id
 const metadata = require('./metadata.json');
 export var trial_name = "";
-export async function getContract() {
+async function getContract() {
     const contract = new ContractPromise(
     await create({ api, pruntimeURL, contractId }), // Phala's "create" decorator
     JSON.parse(metadata),
     contractId);
     return contract;
 }
+export contract = getContract()
 
 export default async function Form() {
 
