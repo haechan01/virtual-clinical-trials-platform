641c540394e0e224c05b8d7cafe43fa0a0759c52
Adakole2020 <78976996+Adakole2020@users.noreply.github.com>
2022-04-04 20:19:20 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -16,6 +16,7 @@ export default async function Results() {
         account,
         signer,
     });
+    contract = getContract()
     var message = ""
     try {
         // obtain stat_test results
